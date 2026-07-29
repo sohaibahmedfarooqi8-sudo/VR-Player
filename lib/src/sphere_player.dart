6398455b25dff871699ead97edd89462523a593a
Sohaib Ahmed Farooqi <sohaibahmedfarooqi8@gmail.com>
2026-07-29 11:24:41 +0500

    Update sphere_player.dart

--- lib/src/sphere_player.dart
+++ lib/src/sphere_player.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/rendering.dart';
 typedef SpherePlayerCreatedCallback = void Function(SpherePlayerController controller);
 
 class SpherePlayer extends StatefulWidget {
