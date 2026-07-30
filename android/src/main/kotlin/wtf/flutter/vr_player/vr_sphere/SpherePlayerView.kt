f77ba1df349ae73452c284097be99cfaec70bb1d
Sohaib Ahmed Farooqi <sohaibahmedfarooqi8@gmail.com>
2026-07-30 16:45:10 +0500

    Update SpherePlayerView.kt

--- android/src/main/kotlin/wtf/flutter/vr_player/vr_sphere/SpherePlayerView.kt
+++ android/src/main/kotlin/wtf/flutter/vr_player/vr_sphere/SpherePlayerView.kt
@@ -68,7 +68,7 @@ class SpherePlayerView(
         }
        if (interactionMode == "motion" || interactionMode == "both") {
     sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)?.let {
-       sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_FASTEST)
+     sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_GAME)
     }
 }
 
@@ -110,7 +110,7 @@ private val remappedRotationMatrix = FloatArray(9)
 private var smoothedYaw = 0f
 private var smoothedPitch = 0f
 private var yawInitialized = false
-private val motionSmoothingFactor = 0.8f
+private val motionSmoothingFactor = 0.10f
     
 override fun onSensorChanged(event: SensorEvent) {
     if (event.sensor.type != Sensor.TYPE_ROTATION_VECTOR) return
