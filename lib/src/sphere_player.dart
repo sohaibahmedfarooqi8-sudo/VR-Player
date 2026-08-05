efd9f44b6b1e3da70db89e2f4dfadc99492f5b1f
Sohaib Ahmed Farooqi <sohaibahmedfarooqi8@gmail.com>
2026-08-05 12:55:25 +0500

    Update sphere_player.dart

--- lib/src/sphere_player.dart
+++ lib/src/sphere_player.dart
@@ -70,13 +70,12 @@ class _SpherePlayerState extends State<SpherePlayer> {
     );
   }
 }
-
 class SpherePlayerController {
   final int viewId;
   late final MethodChannel _channel;
   final _readyCompleter = Completer<int>(); // resolves with duration(ms)
   VoidCallback? onFinished;
-
+  ValueChanged<bool>? onBufferingChanged; // fires for initial load, seek, and any stall
   SpherePlayerController(this.viewId) {
     _channel = MethodChannel('sphere_player_$viewId');
     _channel.setMethodCallHandler((call) async {
@@ -89,10 +88,12 @@ class SpherePlayerController {
         case 'onFinished':
           onFinished?.call();
           break;
+        case 'onBuffering':
+          onBufferingChanged?.call(call.arguments as bool);
+          break;
       }
     });
   }
-
   Future<int> get durationMs => _readyCompleter.future;
   Future<void> play() => _channel.invokeMethod('play');
   Future<void> pause() => _channel.invokeMethod('pause');
